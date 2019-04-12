1bb95910ea14ec2e18b652eb4b33754becf50b4d
Jan Poniatowski <poniatowski126@gmail.com>
2019-04-12 10:24:38 +0200

    more plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
     module: {
         rules: [{
@@ -20,18 +22,33 @@ module.exports = {
         },
         {
             test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: ["css-loader", 'sass-loader']
+            })
+          },
+        {
+            test: /\.(png|jpg|gif)$/,
             use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }
+              {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img/',
+                    publicPath: './img',
+                    useRelativePaths: true
+                },
+              },
+            ],
+        } 
     ]
     },
     
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new ExtractTextPlugin("css/style.css"),
+        new CleanWebpackPlugin(),
     ]
 };

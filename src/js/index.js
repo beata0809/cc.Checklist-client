948b9d60ad7a205812ab709fb181fdf8daf3515d
Rafał Wolny <rafal.wolny96@gmail.com>
2019-04-17 17:33:33 +0200

    Pasek nawigacji

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,12 @@ import '../scss/style.scss';
 import "@babel/polyfill";
 import "bootstrap";
 import Api from './Api/Api';
+import UserBar from './UserBar';
+
+const userBar = new UserBar('#UserBar');
 
 window.onload = () => {
     console.log('Hello World');
     Api.fetchExample();
+    userBar.render();
 }

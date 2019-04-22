eed09c6a0e59c7e614d70df765b0de574c451a63
Pola <polaskibinska@gmail.com>
2019-04-22 14:53:42 +0200

    LoginPage

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,15 @@ import "@babel/polyfill";
 import "bootstrap";
 import Api from './Api/Api';
 import UserBar from './UserBar';
+import LoginPage from './LoginPage';
 
 const userBar = new UserBar('#UserBar');
+const loginPage = new LoginPage('LoginPage');
 
 window.onload = () => {
     console.log('Hello World');
     Api.fetchExample();
     userBar.render();
+
+    //loginPage.render();
 }

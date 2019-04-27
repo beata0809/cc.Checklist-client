563bb68fd83601952c4e675e5ae7c8ef29a415b9
Risusagi <anastazja199707@gmail.com>
2019-04-27 21:56:13 +0200

    Wyświetlanie task'ów i przycisk do dodawania

--- src/js/MainPannel/List.js
+++ src/js/MainPannel/List.js
@@ -23,11 +23,27 @@ class List {
         <h2>${data.title}</h2>
           <div class='tasks'>
           </div>
+        <button type="button" class="btn btn-outline-dark">
+          Dodaj
+        </button>
+        <input type="textarea">
         `;
 
     // Trzeba dodac taski do div'a .tasks
     // wzor taska "<input type="checkbox">Task example</input><br>"
+    data.tasks.forEach(task => {
+      const showTask = document.createElement('div');
+        showTask.innerHTML = `
+        <input type="checkbox">${task.name}</input>
+        <br>
+        `;
+      element.querySelector('.tasks').appendChild(showTask);
+    })
 
+
+    element.querySelector('button').addEventListener('click', () => {
+      element.querySelector('[type=textarea]').style.display = 'initial';
+    })
     document.querySelector('.lists').appendChild(element);
   }
 }

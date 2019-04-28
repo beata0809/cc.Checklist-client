22b71af6bdd35553c4349008c51fe5b636e95305
Risusagi <anastazja199707@gmail.com>
2019-04-28 11:23:42 +0200

    Wyniesienie logiki poza render

--- src/js/MainPannel/List.js
+++ src/js/MainPannel/List.js
@@ -12,9 +12,40 @@ class List {
 
   // dodanie do bazy
   static async addTask(input) {
+    const taskName = input.value;
+    console.log(taskName);
+    this.render(taskName);
+  }
+  static changeStatus (showTask, task, data, element) {
+    //wykreślanie i zmiana statusu task'ów
+    const checkbox = showTask.querySelector('[type=checkbox]');
+    checkbox.addEventListener('click', () => {
+      if (checkbox.checked) {
+        showTask.style.textDecoration = 'line-through';
+        task.done = true;
+
+      } else {
+        showTask.style.textDecoration = 'none';
+        task.done = false;
+      }
+       console.log(task);
+      listStatusCheck();
+    });
+
+    //status i znikanie list
+    const listStatusCheck = () => {
+      let listStatus = data.tasks.every(el => {
+        return el.done === true;
+      });
+
+      if (listStatus) {
+        //element.style.display = 'none'; //inaczej będzie znikać po zaznaczeniu tego jednego tasku
+      } else {
+        element.style.display = '';
+      }
+    };
 
   }
-
   // render pojedynczej listy
   static render(data) {
     const titleNoSpaces = data.titleNoSpaces;
@@ -44,52 +75,37 @@ class List {
 
     // Trzeba dodac taski do div'a .tasks
     // wzor taska "<input type="checkbox">Task example</input><br>"
-    data.tasks.forEach(task => {
-      const showTask = document.createElement('div');
+    console.log(data.tasks);
+
+      data.tasks.forEach(task => {
+        const showTask = document.createElement('div');
         showTask.innerHTML = `
         <input type="checkbox">${task.name}</input>
         <br>
         `;
-      element.querySelector('.tasks').appendChild(showTask);
+        element.querySelector('.tasks').appendChild(showTask);
+        this.changeStatus(showTask, task, data, element);
 
-       //wykreślanie i zmiana statusu task'ów
-      const checkbox = showTask.querySelector('[type=checkbox]');
-      checkbox.addEventListener('click', () => {
-        if (checkbox.checked) {
-          showTask.style.textDecoration = 'line-through';
-          task.done = true;
-        } else {
-            showTask.style.textDecoration = 'none';
-            task.done = false;
-        }
-        listStatusCheck();
-      });
-    })
-
-    //status i znikanie list
-    const listStatusCheck = () => {
-      let listStatus = data.tasks.every(el => {
-        return el.done === true;
-      });
-
-      if (listStatus) {
-        //element.style.display = 'none'; //inaczej będzie znikać po zaznaczeniu tego jednego tasku
-      } else {
-        element.style.display = '';
-      }
-    };
+      })
 
 
     element.querySelector('button').addEventListener('click', () => {
       // utworzenie nowego taska
       const task = document.createElement('input');
       task.setAttribute=('type', 'checkbox');
+      const textInput = document.querySelector(`#${titleNoSpaces}-input`)
+      this.addTask(textInput);
       // TODO
       // pobranie tekstu z inputa
+
+        //console.log(document.querySelectorAll(`#${titleNoSpaces}-input`)[0].value);
+
+      //console.log(document.querySelector(`#${titleNoSpaces}-input`))
       // dolaczenie go do listy
       // dolaczenie go do bazy
 
     })
+
     document.querySelector('.lists').appendChild(element);
   }
 
